6b29f1b615d7eb932c37c9d221ede2cea023ba0d
Emmanuel <emmopk11@outlook.com>
2018-10-21 22:01:02 +0200

    contact forms centered

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,15 +11,12 @@ const Contact = ({resume}) => (
         <p className="section-subtitle"></p>
       </header>
       <div className="row">
-        <div className="col-sm-5 contact-info">
-          <h3>Contact Info</h3>
-          <p><i className="fa fa-map-marker"></i> {resume.address} </p>
-          <p><i className="fa fa-phone"></i> (555) 123-4567</p>
-          <p><i className="fa fa-envelope-o"></i> {resume.email}</p>
-        </div>
-        <div className="col-sm-7">
-          <h3>Get in Touch</h3>
-          <ContactForm />
+        <div className="main-contact">
+          <div id="contacts">
+            <h3>Get in Touch</h3>
+            <p><i className="fa fa-envelope-o"></i> {resume.email}</p>
+            <ContactForm />
+          </div>
         </div>
       </div>
     </div>
